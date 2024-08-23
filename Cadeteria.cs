7728a15dffa83b163ff91230d15cc99455ff58bd
Rodrigo <rodrigomendoza721@gmail.com>
2024-08-23 19:57:15 -0300

    cambios pedidos y cadeteria

--- Cadeteria.cs
+++ Cadeteria.cs
@@ -7,8 +7,13 @@ namespace cadeteria
         private string tel;
         private List<Cadete> listaCadetes;
 
+        public Cadeteria()
+        {
+            this.listaCadetes = new List<Cadete>();
+        }
+
         public string Nombre { get => nombre; set => nombre = value; }
         public string Tel { get => tel; set => tel = value; }
-        public List<Cadete> ListaCadetes { get => listaCadetes; set => listaCadetes = value; }
+    
     }
 }
--- Pedidos.cs
+++ Pedidos.cs
@@ -1,14 +1,30 @@
 using cliente;
 namespace pedidos
 {
+    enum Estado
+    {
+        procesamiento,
+        enCamino,
+        perdido,
+        entregado
+    }
     public class Pedido
     {
         private int nro;
         private string obs;
         private Cliente cliente;
 
+        public Pedido(string nombre, string telefono, string direccion, string referencia)
+        {
+            this.cliente = new Cliente(){Nombre = nombre, Telefono = telefono, Direccion = direccion, Referencia = referencia };
+        }
+
+        private enum Estado;
+
         public int Nro { get => nro; set => nro = value; }
         public string Obs { get => obs; set => obs = value; }
-        public Cliente Cliente { get => cliente; set => cliente = value; }
+        public string verDireccionCliente(){
+            return cliente.Direccion;
+        }
     }
 }
